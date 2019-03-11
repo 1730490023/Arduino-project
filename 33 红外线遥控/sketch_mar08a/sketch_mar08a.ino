d729e19a1a7194852523b9098d7a70da2f5c8a0d
1730490023 <38779328+1730490023@users.noreply.github.com>
2019-03-11 17:51:16 +0800

    Create sketch_mar08a.ino

---
+++ 33 红外线遥控/sketch_mar08a/sketch_mar08a.ino
@@ -0,0 +1,25 @@
+#include <IRremote.h>
+
+int RECV_PIN = 11;          //红外接收引脚定义
+
+IRrecv irrecv(RECV_PIN);
+
+decode_results results;
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Enabling IRin");
+  irrecv.enableIRIn(); // 开始接收
+  Serial.println("Enabled IRin");
+}
+
+void loop() 
+{
+  if (irrecv.decode(&results)) 
+  {
+    Serial.println(results.value, HEX);
+    irrecv.resume();    // 接收下一个值
+  }
+  delay(100);
+}
